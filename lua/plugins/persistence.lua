40d58ace9cfb2ad81de7061ba07b99059e0e264e
Jay Madden <jaymaddencox@gmail.com>
2024-01-15 16:28:35 -0600

    switch to persistence for session

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,9 +1,9 @@
 return {
-	"Pocco81/auto-save.nvim",
-	enabled = true,
+	"folke/persistence.nvim",
+  event = "BufReadPre", -- this will only start session saving when an actual file was opened
 
 	config = function()
-		require("auto-save").setup({
+		require("persistence").setup({
 			dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"), -- directory where session files are saved
 			options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
 			pre_save = nil, -- a function to call before saving the session
