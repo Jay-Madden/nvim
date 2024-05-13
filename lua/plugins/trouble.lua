010d7a82af6953eb6935a5c87be195439caacfa1
Jay Cox <jay.cox@aa.com>
2024-05-13 13:14:20 -0500

    fix trouble config for now, remove telescope

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -20,18 +20,6 @@ return {
   },
 
   config = function()
-    local actions = require("telescope.actions")
-    local open_with_trouble = require("trouble.sources.telescope").open
-
-    local telescope = require("telescope")
-
-    telescope.setup({
-      defaults = {
-        mappings = {
-          i = { ["<c-t>"] = open_with_trouble },
-          n = { ["<c-t>"] = open_with_trouble },
-        },
-      },
-    })
+    require("trouble").setup({})
   end,
 }
