ca2120ce04480fafd3a12f8aa56b8842bdc52626
Jay Cox <jay.cox@aa.com>
2024-07-17 13:09:59 -0500

    fix status column and add gitsign support

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -3,39 +3,48 @@ return {
 
   config = function()
     local builtin = require("statuscol.builtin")
-    require("statuscol").setup({
-      relculright = true,
-      segments = {
-        {
-          sign = {
-            name = { "[DapBreakpoint|Marks*]" },
-            maxwidth = 1,
-          },
-          click = "v:lua.ScSa",
+    local segments = {
+      {
+        sign = {
+          name = { "[DapBreakpoint|Marks*]" },
+          maxwidth = 1,
         },
-        {
-          sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
-          click = "v:lua.ScSa",
-        },
-        { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-        {
-          sign = {
-            namespace = { "gitsign" },
-            maxwidth = 1,
-          },
-          click = "v:lua.ScSa",
-        },
-        {
-          sign = {
-            name = { ".*" },
-            maxwidth = 1,
-            colwidth = 1,
-            wrap = true,
-          },
-          click = "v:lua.ScSa",
-        },
+        click = "v:lua.ScSa",
       },
+      {
+        sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true },
+        click = "v:lua.ScSa",
+      },
+      { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+      { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+    }
+
+    -- Check if the current directory is a git repo, if it is show the gitsigns in the gutter
+    local current_rev = vim.fn.system("git rev-parse --show-toplevel 2> /dev/null")
+    if current_rev ~= "" then
+      table.insert(segments, {
+        sign = {
+          namespace = { "gitsign" },
+          maxwidth = 1,
+        },
+        click = "v:lua.ScSa",
+      })
+    end
+
+    table.insert(segments, {
+      sign = {
+        name = { ".*" },
+        maxwidth = 1,
+        colwidth = 1,
+        wrap = true,
+      },
+      click = "v:lua.ScSa",
     })
+
+    local setup_table = {
+      relculright = true,
+      segments = segments,
+    }
+    require("statuscol").setup(setup_table)
   end,
 }
