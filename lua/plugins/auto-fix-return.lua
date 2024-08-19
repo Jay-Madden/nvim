d13d18d2f9b5d8704e9e2f9a03fc71a497b71611
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2024-08-19 07:58:57 -0500

    update auto-fix-return to master branch

--- lua/plugins/auto-fix-return.lua
+++ lua/plugins/auto-fix-return.lua
@@ -1,6 +1,5 @@
 return {
   "Jay-Madden/auto-fix-return.nvim",
-  branch = "update-query-terminate-shortvar",
   config = function()
     require("auto-fix-return").setup({
       enable_autocmds = true,
