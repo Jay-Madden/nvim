02b18fe9db436822c3360e9ad54482ad2bfabba1
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2024-09-21 14:15:48 -0500

    fix: turn off lazy checker notification

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,22 +18,21 @@ return {
 
   config = function()
     local cmp = require("cmp")
-    local lspkind = require('lspkind')
+    local lspkind = require("lspkind")
     cmp.setup({
       snippet = {
         expand = function(args)
           require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
         end,
       },
-        ---@diagnostic disable-next-line: missing-fields
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = 'symbol_text', -- show only symbol annotations
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-          })
-        },
-
+      ---@diagnostic disable-next-line: missing-fields
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text", -- show only symbol annotations
+          ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+        }),
+      },
 
       window = {
         completion = cmp.config.window.bordered({ border = "rounded" }),
--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -8,20 +8,38 @@ return {
 
   config = function()
     require("neoscroll").setup({})
-    local neoscroll = require('neoscroll')
+    local neoscroll = require("neoscroll")
     local keymap = {}
 
-    keymap["<C-u>"] = function() neoscroll.ctrl_u({duration = 20}) end
-    keymap["<C-d>"] = function() neoscroll.ctrl_d({duration = 20}) end
-    keymap["<C-b>"] = function() neoscroll.ctrl_b({duration = 20}) end
-    keymap["<C-f>"] = function() neoscroll.ctrl_f({duration = 20}) end
-    keymap["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor=false; duration = 20 }) end;
-    keymap["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor=false; duration = 20 }) end;
-    keymap["zt"]    = function() neoscroll.zt({ half_win_duration = 50 }) end;
-    keymap["zz"]    = function() neoscroll.zz({ half_win_duration = 50 }) end;
-    keymap["zb"]    = function() neoscroll.zb({ half_win_duration = 50 }) end;
+    keymap["<C-u>"] = function()
+      neoscroll.ctrl_u({ duration = 20 })
+    end
+    keymap["<C-d>"] = function()
+      neoscroll.ctrl_d({ duration = 20 })
+    end
+    keymap["<C-b>"] = function()
+      neoscroll.ctrl_b({ duration = 20 })
+    end
+    keymap["<C-f>"] = function()
+      neoscroll.ctrl_f({ duration = 20 })
+    end
+    keymap["<C-y>"] = function()
+      neoscroll.scroll(-0.1, { move_cursor = false, duration = 20 })
+    end
+    keymap["<C-e>"] = function()
+      neoscroll.scroll(0.1, { move_cursor = false, duration = 20 })
+    end
+    keymap["zt"] = function()
+      neoscroll.zt({ half_win_duration = 50 })
+    end
+    keymap["zz"] = function()
+      neoscroll.zz({ half_win_duration = 50 })
+    end
+    keymap["zb"] = function()
+      neoscroll.zb({ half_win_duration = 50 })
+    end
 
-    local modes = { 'n', 'v', 'x' }
+    local modes = { "n", "v", "x" }
     for key, func in pairs(keymap) do
       vim.keymap.set(modes, key, func)
     end
