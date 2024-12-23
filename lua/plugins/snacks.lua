c365bc34604c81909e32d4e98435d5e495ca7f3f
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2024-12-23 14:17:20 -0500

    chore: shorten colorscript script

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,39 +1,25 @@
 -- Taken from https://gitlab.com/dwt1/shell-color-scripts/-/blob/master/colorscripts/square
 local colorscript_square = [[
-# ANSI Color -- use these variables to easily have different color
-#    and format output. Make sure to output the reset sequence after 
-#    colors (f = foreground, b = background), and use the 'off'
-#    feature for anything you turn on.
-# Author: muzieca
-# Source: http://crunchbang.org/forums/viewtopic.php?pid=127509#p127509
-initializeANSI()
-{
-  esc=""
+esc=""
 
-  blackf="${esc}[30m";   redf="${esc}[31m";    greenf="${esc}[32m"
-  yellowf="${esc}[33m"   bluef="${esc}[34m";   purplef="${esc}[35m"
-  cyanf="${esc}[36m";    whitef="${esc}[37m"
-  
-  blackfbright="${esc}[90m";   redfbright="${esc}[91m";    greenfbright="${esc}[92m"
-  yellowfbright="${esc}[93m"   bluefbright="${esc}[94m";   purplefbright="${esc}[95m"
-  cyanfbright="${esc}[96m";    whitefbright="${esc}[97m"
-  
-  blackb="${esc}[40m";   redb="${esc}[41m";    greenb="${esc}[42m"
-  yellowb="${esc}[43m"   blueb="${esc}[44m";   purpleb="${esc}[45m"
-  cyanb="${esc}[46m";    whiteb="${esc}[47m"
+blackf="${esc}[30m";   redf="${esc}[31m";    greenf="${esc}[32m"
+yellowf="${esc}[33m"   bluef="${esc}[34m";   purplef="${esc}[35m"
+cyanf="${esc}[36m";    whitef="${esc}[37m"
 
-  boldon="${esc}[1m";    boldoff="${esc}[22m"
-  italicson="${esc}[3m"; italicsoff="${esc}[23m"
-  ulon="${esc}[4m";      uloff="${esc}[24m"
-  invon="${esc}[7m";     invoff="${esc}[27m"
+blackfbright="${esc}[90m";   redfbright="${esc}[91m";    greenfbright="${esc}[92m"
+yellowfbright="${esc}[93m"   bluefbright="${esc}[94m";   purplefbright="${esc}[95m"
+cyanfbright="${esc}[96m";    whitefbright="${esc}[97m"
 
-  reset="${esc}[0m"
-}
+blackb="${esc}[40m";   redb="${esc}[41m";    greenb="${esc}[42m"
+yellowb="${esc}[43m"   blueb="${esc}[44m";   purpleb="${esc}[45m"
+cyanb="${esc}[46m";    whiteb="${esc}[47m"
 
-# note in this first use that switching colors doesn't require a reset
-# first - the new color overrides the old one.
+boldon="${esc}[1m";    boldoff="${esc}[22m"
+italicson="${esc}[3m"; italicsoff="${esc}[23m"
+ulon="${esc}[4m";      uloff="${esc}[24m"
+invon="${esc}[7m";     invoff="${esc}[27m"
 
-initializeANSI
+reset="${esc}[0m"
 
 cat << EOF
 
