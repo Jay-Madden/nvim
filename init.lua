7f90579534ec5bde9a8f71a7912fb276b2c8a0e4
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2024-12-23 16:23:55 -0500

    fix: backport highlight name for 0.10.3 regression
    references: https://github.com/neovim/neovim/pull/31676

--- init.lua
+++ init.lua
@@ -9,6 +9,10 @@ end
 vim.print = _G.dd
 -----------------------------------
 
+-- Temp fix for 0.10.3 regression with :inspect command
+-- https://github.com/neovim/neovim/issues/31675#issuecomment-2558405042
+vim.hl = vim.highlight
+
 -- Initialize initial configuration
 require("config.options")
 
