a46f0a5232c164f48949caacd8b6f9202e5dd728
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2025-01-19 10:57:27 -0600

    fix: remove auto save message

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -4,6 +4,11 @@ return {
 
   config = function()
     require("auto-save").setup({
+      execution_message = {
+        message = function()
+          return ""
+        end,
+      },
       dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"), -- directory where session files are saved
       options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
       pre_save = nil, -- a function to call before saving the session
