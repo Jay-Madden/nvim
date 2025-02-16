c8c96447b290d1ce874ade7fdfc94dc71c8f92ec
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2025-02-15 22:38:37 -0600

    chore: mess with some plugin load events to improve startup time

--- lua/plugins/actions-preview.lua
+++ lua/plugins/actions-preview.lua
@@ -1,5 +1,7 @@
 return {
   "aznhe21/actions-preview.nvim",
+  event = "VeryLazy",
+
   config = function()
     vim.keymap.set("n", "<leader>hh", require("actions-preview").code_actions)
   end,
--- lua/plugins/goto-preview.lua
+++ lua/plugins/goto-preview.lua
@@ -1,5 +1,7 @@
 return {
   "rmagatti/goto-preview",
+  event = "VeryLazy",
+
   config = function()
     require("goto-preview").setup({
       default_mappings = true,
