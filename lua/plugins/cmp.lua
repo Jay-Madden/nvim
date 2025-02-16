c019002dd7c5b56bc84a36fcaa51cfe8d5673552
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2025-02-15 22:25:19 -0600

    feat: add colorful-menu to nvim-cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -14,6 +14,7 @@ return {
         require("luasnip").setup()
       end,
     },
+    "xzbdmw/colorful-menu.nvim",
   },
 
   config = function()
@@ -31,6 +32,19 @@ return {
           mode = "symbol_text", -- show only symbol annotations
           ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
           show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+          before = function(entry, vim_item)
+            local highlights_info = require("colorful-menu").cmp_highlights(entry)
+
+            -- highlight_info is nil means we are missing the ts parser, it's
+            -- better to fallback to use default `vim_item.abbr`. What this plugin
+            -- offers is two fields: `vim_item.abbr_hl_group` and `vim_item.abbr`.
+            if highlights_info ~= nil then
+                vim_item.abbr_hl_group = highlights_info.highlights
+                vim_item.abbr = highlights_info.text
+            end
+
+            return vim_item
+          end,
         }),
       },
 
