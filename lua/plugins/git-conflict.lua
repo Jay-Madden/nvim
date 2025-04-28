a25c41a113d2b54b8f09dfddc634b48d112360e1
Jay Madden <jaymaddencox@gmail.com>
2025-04-28 11:59:51 -0500

    fix: add highlighting hack for conflict

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -3,6 +3,9 @@ return {
   event = "VeryLazy",
 
   config = function()
-    require("git-conflict").setup()
+    require("git-conflict").setup({})
+    -- HACK: taken from here: https://github.com/akinsho/git-conflict.nvim/pull/20#issue-1259829668
+    vim.api.nvim_set_hl(0, 'DiffText', { fg = "#ffffff", bg = "#1d3b40" })
+    vim.api.nvim_set_hl(0, 'DiffAdd', { fg = "#ffffff", bg = "#1d3450" })
   end,
 }
