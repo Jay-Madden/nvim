3dd1be536e104adfbcfe678a43d10b62fee21768
Jay Madden <jaymaddencox@gmail.com>
2025-10-07 18:35:20 -0500

    chore: update and enable copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
   event = { "InsertEnter" },
-  enabled = false,
+  enabled = true,
   config = function()
     require("copilot").setup({
       suggestion = {
