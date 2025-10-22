89d532406fb1ad9657fd24dcfe2d636bc4af4438
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2025-10-21 22:23:19 -0500

    chore: only load copilot if cls is present

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
   event = { "InsertEnter" },
-  enabled = true,
+  enabled = function() vim.fn.executable("copilot-language-server") end,
   config = function()
     require("copilot").setup({
       suggestion = {
