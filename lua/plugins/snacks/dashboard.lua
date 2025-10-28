5b3ffe7d228e8978d77bd0a60c73298a9a8739df
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2025-10-27 22:21:03 -0500

    feat: update dashboard sections and some plugins

--- lua/plugins/snacks/dashboard.lua
+++ lua/plugins/snacks/dashboard.lua
@@ -22,7 +22,6 @@ return {
               })
             end,
           },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           {
             icon = " ",
             key = "g",
@@ -32,22 +31,9 @@ return {
             end,
           },
           {
-            icon = " ",
-            key = "r",
-            desc = "Recent Files",
-            action = ":lua Snacks.dashboard.pick('oldfiles')",
-          },
-          {
-            icon = " ",
-            key = "c",
-            desc = "Config",
-            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-          },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          {
             icon = "󰒲 ",
             key = "L",
-            desc = "Lazy",
+            desc = "Plugins",
             action = ":Lazy",
             enabled = package.loaded.lazy ~= nil,
           },
